77fdc240126b08329f620bad0f29465f6c88a7c9
arronrees <64908445+arronrees@users.noreply.github.com>
2020-08-10 19:38:42 +0100

    12.0 Copy to clipboard

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,7 @@ const colorDivs = document.querySelectorAll('.color');
 const generateBtn = document.querySelector('.generate');
 const sliders = document.querySelectorAll('input[type="range"]');
 const currentHexes = document.querySelectorAll('.color h2');
+const popup = document.querySelector('.copy-container');
 let initialColors;
 
 // Event Listeners
@@ -15,6 +16,18 @@ colorDivs.forEach((div, index) => {
   div.addEventListener('change', () => {
     updateTextUI(index);
   });
+});
+
+currentHexes.forEach((hex) => {
+  hex.addEventListener('click', () => {
+    copyToClipboard(hex);
+  });
+});
+
+popup.addEventListener('transitionend', () => {
+  const popupBox = popup.children[0];
+  popup.classList.remove('active');
+  popupBox.classList.remove('active');
 });
 
 // Functions
@@ -148,6 +161,19 @@ function resetInputs() {
       slider.value = Math.floor(satValue * 100) / 100;
     }
   });
+}
+
+function copyToClipboard(hex) {
+  const el = document.createElement('textarea');
+  el.value = hex.innerText;
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+  // Popup Animation
+  const popupBox = popup.children[0];
+  popup.classList.add('active');
+  popupBox.classList.add('active');
 }
 
 randomColors();
